6df0dd78af4c4a2eb17cbf1c7eac70793dc4f6ff
EvgeniaGerts <evgeniya@cobalt.io>
2022-04-26 16:26:58 +0200

    Generic integration

--- app2/container/src/App.js
+++ app2/container/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { mount } from "marketing/MarketingApp";
+import MarkeringApp from "./components/MarkeringApp";
 
-console.log(mount);
-
-export default () => <h1>Hi there</h1>;
+export default () => (
+  <div>
+    <h1>Hi there</h1>
+    <hr />
+    <MarkeringApp />
+  </div>
+);
