67228891b16526a44552700a38ba73580c34864e
EvgeniaGerts <evgeniya@cobalt.io>
2022-05-12 18:47:42 +0200

    Delegating shared modules

--- app2/container/config/webpack.dev.js
+++ app2/container/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common.js");
+const packageJson = require("../package.json");
 
 const devConfig = {
   mode: "development",
@@ -17,10 +18,7 @@ const devConfig = {
       remotes: {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
-      shared: ["react", "react-dom"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      shared: packageJson.dependencies,
     }),
   ],
 };
